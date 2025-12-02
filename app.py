8b25f04285dfc134dfd1c7785d567e3ac0d7c71d
Marcel Friesen <mafriese.cc@gmail.com>
2025-12-02 19:38:24 +0100

    doing google cloud stuff

--- app.py
+++ app.py
@@ -77,4 +77,6 @@ def reveal(token):
     return render_template('reveal.html', giver=giver, receivers=receivers)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
